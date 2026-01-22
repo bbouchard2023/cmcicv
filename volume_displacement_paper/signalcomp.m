1f370b6fdbd47b1e4eef5e1cab04f75207f991a3
--global <--global>
2026-01-22 16:40:05 -0500

    Updated files to match new publication goals

---
+++ volume_displacement_paper/signalcomp.m
@@ -0,0 +1,21 @@
+%==========================================================================
+%% Signal Comparison
+%
+% Compares displacement between computational model and SCG data, and
+% evaluates correlation between ventricular volume and chest displacement
+% 
+%
+% Brendan Bouchard
+% 20260122
+% Last Updated: 20260122
+%==========================================================================
+
+clc
+clear
+close all
+
+%% Model/SCG Displacement Comparison
+
+
+
+%% Ventricular Volume/Chest Displacement Correlation
