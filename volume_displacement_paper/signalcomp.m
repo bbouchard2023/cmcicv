caca39ad96c2a845ba6abc356b04768f4d45f1bf
--global <--global>
2026-01-26 16:58:20 -0500

    Created comparison between cardiac volume and chest displacement

--- volume_displacement_paper/signalcomp.m
+++ volume_displacement_paper/signalcomp.m
@@ -30,3 +30,34 @@ legend("Computational Model Displacement")
 
 
 %% Ventricular Volume/Chest Displacement Correlation
+
+files = dir("volume*.mat");
+frameNum = arrayfun(@(f) sscanf(f.name,'volume%d.mat'), files);
+[frameNum, idx] = sort(frameNum);
+files = files(idx);
+numfiles = numel(files);
+vol = zeros(numfiles,1);
+dt = 0.034;
+
+for l = 1:numfiles
+    S = load(files(l).name);
+    J = S.J;
+    vol(l) = nnz(J);
+end
+
+t = (frameNum - frameNum(1)) * dt;
+
+fig2 = figure;
+fig2.WindowState = "maximized";
+subplot(2,1,1)
+plot(t,vol,'m');
+title("Cardiac Volume")
+xlabel("Time (s)")
+ylabel("Volume (mL)")
+xlim([t(1) ansys_t(end)])
+subplot(2,1,2)
+plot(ansys_t,ansys_disp,"g")
+title("Computational Model Displacement")
+xlabel("Time (s)")
+ylabel("Displacement (m)")
+
